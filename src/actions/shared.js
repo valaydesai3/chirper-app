ab5c73b44479b54875351437b12d3b16867e75f1
Valay Desai <chako.pakodi@gmail.com>
2020-08-21 12:59:57 -0400

    first commit

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { getInitialData } from '../utils/api';
+import { receiveTweets } from './tweets';
+import { receiveUsers } from './users';
+import { setAuthedUser } from './authedUser';
+
+const AUTHED_ID = 'tylermcginnis';
+
+export function handleInitialData() {
+  return (dispatch) => {
+    return getInitialData().then(({ users, tweets }) => {
+      dispatch(receiveTweets(tweets));
+      dispatch(receiveUsers(users));
+      dispatch(setAuthedUser(AUTHED_ID));
+    });
+  };
+}
