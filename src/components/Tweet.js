15fbceb959adc9e3202c0b9a93f9ada717e3d50a
Valay Desai <chako.pakodi@gmail.com>
2020-08-21 17:38:25 -0400

    Liking a tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -6,12 +6,20 @@ import {
   TiHeartOutline,
   TiHeartFullOutline,
 } from 'react-icons/ti/index';
+import { handleToggleTweet } from '../actions/tweets';
 
 class Tweet extends Component {
   handleLike = (e) => {
     e.preventDefault();
 
-    // todo: Handle Like Tweet
+    const { dispatch, tweet, authedUser } = this.props;
+    dispatch(
+      handleToggleTweet({
+        id: tweet.id,
+        hasLiked: tweet.hasLiked,
+        authedUser,
+      })
+    );
   };
   toParent = (e, id) => {
     e.preventDefault();
